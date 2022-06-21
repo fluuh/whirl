82e9ca4ded67775fcbf81aeab7349a0e1b47f39b
fluuh <102534479+fluuh@users.noreply.github.com>
2022-06-21 16:51:31 +0200

    wrl_type

--- wrl/common.h
+++ wrl/common.h
@@ -3,6 +3,15 @@
 
 #define WRL_LIST_DEFAULT 128
 
+typedef enum {
+	wrl_void,
+	wrl_list,
+	wrl_func,
+	wrl_table, /* TODO */
+	wrl_number,
+	wrl_string,
+} wrl_type;
+
 /* used by most files */
 #include <stdlib.h>
 #include <string.h>
