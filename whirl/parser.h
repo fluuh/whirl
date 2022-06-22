8cf12233f8fdaf17b8f93eeaa63c3950eecd7934
fluuh <102534479+fluuh@users.noreply.github.com>
2022-06-22 12:11:29 +0200

    update parser.h

--- whirl/parser.h
+++ whirl/parser.h
@@ -7,10 +7,10 @@
 #ifndef WRL_PARSER_H_
 #define WRL_PARSER_H_
 
-#include "ir.h"
+#include "tir.h"
 #include "value.h"
 
-tir_unit wrl_parse(const char *src);
-tir_unit wrl_parse_list(wrl_value_t list);
+tir_module *wrl_parse(const char *src);
+tir_module *wrl_parse_list(wrl_value_t list);
 
 #endif
