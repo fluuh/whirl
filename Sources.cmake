05ef2f2fdd2b36268d2dcf3a4ae028eac776b0af
fluuh <102534479+fluuh@users.noreply.github.com>
2022-06-25 23:12:45 +0200

    nvm whirl is for the cli

--- Sources.cmake
+++ Sources.cmake
@@ -1,11 +1,11 @@
 target_sources(whirl PRIVATE
-  whirl/build.c
-  whirl/builtin.c
-  whirl/reader.c
-  whirl/tir.c
-  whirl/value.c
-  whirl/list.c
-  whirl/util.c)
+  wrl/build.c
+  wrl/builtin.c
+  wrl/reader.c
+  wrl/tir.c
+  wrl/value.c
+  wrl/list.c
+  wrl/util.c)
 
 target_sources(whirl.cli PRIVATE
-  whirl/wrl-main.c)
+  wrl/wrl-main.c)
